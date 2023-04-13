c14c75ba5b11f44b2e525cfc05e95c9ceb2f377a
Mateus Ceni <mateus.ceni@gmail.com>
2023-04-12 21:24:43 -0300

    aula2

--- src/server.js
+++ src/server.js
@@ -2,5 +2,20 @@ const express = require("express");
 
 const app = express();
 
+app.get("/message/:id/:user", (request, response) => {
+    const {id, user } = request.params;
+    response.send(`
+    Id da mensagem: ${id}.
+    Para o usuário: ${user}.
+    `);
+
+});
+
+app.get("/users", (request, response) => {
+    const {page, limit} = request.query;
+
+    response.send(`Pagina: ${page}. Mostrar:${limit}`);
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server in running on Port ${PORT}`));
